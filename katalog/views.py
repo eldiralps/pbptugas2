01e08c69340e1faf2614c7e0f35962553be747a4
eldiralps <eldiralps@gmail.com>
2022-09-14 14:42:05 +0700

    done VMT

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    daftar_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': daftar_katalog,
+    'nama': 'Eldira Lahanny Permata Sherman'
+    }
+    return render(request, "katalog.html", context)
