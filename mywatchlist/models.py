cac888edab8c7795230894435b82f3aa2aa00b78
eldiralps <eldiralps@gmail.com>
2022-09-22 12:08:01 +0700

    fix data problem

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class MyWatchList(models.Model):
     watched = models.CharField(max_length=50) 
     title = models.CharField(max_length=100) 
-    rating = models.CharField(max_length=50)
+    rating = models.IntegerField()
     release_date = models.CharField(max_length=50)
     review = models.TextField()
